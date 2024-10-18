a1322f86c673d06e2ce01974a36989d712096a04
winstonjose01 <winston.jose@gmail.com>
2024-10-17 18:44:39 -0700

    revision-1: adding comments

--- 210-midterm-starter-1.cpp
+++ 210-midterm-starter-1.cpp
@@ -1,28 +1,32 @@
+// COMSC210 | Midterm 1 | Winston Jose
+// IDE used: Visual Studio Code
+// Github link: https://github.com/winstonjose01/COMSC210-Midterm1
+
 #include <iostream>
 using namespace std;
 
 const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
 
-class DoublyLinkedList {
+class DoublyLinkedList {    // Defines the class for the doubly linked list
 private:
-    struct Node {
-        int data;
-        Node* prev;
-        Node* next;
-        Node(int val, Node* p = nullptr, Node* n = nullptr) {
-            data = val; 
-            prev = p;
-            next = n;
+    struct Node {           // Defines the struct for a node in the linked list
+        int data;           // Integer data contained in a node in the list
+        Node* prev;         // Pointer to the previous node on the list
+        Node* next;         // Pointer to the next node on the list
+        Node(int val, Node* p = nullptr, Node* n = nullptr) {      // Constructor to initialize the node
+            data = val;     // Initialize the data variable with val parameter
+            prev = p;       // Initialize the previous pointer with null pointer
+            next = n;       // Initialize the next pointer with the null pointer
         }
     };
 
-    Node* head;
-    Node* tail;
+    Node* head;     // Pointer to the head or first node of the linked list
+    Node* tail;     // Pointer to the tail or last node of the linked list
 
 public:
-    DoublyLinkedList() { head = nullptr; tail = nullptr; }
+    DoublyLinkedList() { head = nullptr; tail = nullptr; }  // Constructor to initialize an empty list
 
-    void insert_after(int value, int position) {
+    void insert_after(int value, int position) {            // Method to insert a new node at a given position
         if (position < 0) {
             cout << "Position must be >= 0." << endl;
             return;
